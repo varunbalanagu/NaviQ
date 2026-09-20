93d2b41a5f26fa69390412a175621db3ccbb24f2
BALANAGU VARUN <varunbalanagu@gmail.com>
2026-09-20 17:34:55 +0530

    Fix factory contract compilation

--- Low level design/FactoryClass.java
+++ Low level design/FactoryClass.java
@@ -1,21 +1,17 @@
-interface Factory{
-    // void provide();
-}
-// A factory design of all the safe stations 
 public class FactoryClass implements SafeRouteFactory {
-    public   Factory createFactory(String type){
-        if(type.equals("Hospital")){
+    public Factory createFactory(String type){
+        if("Hospital".equals(type)){
             return new Hospital("Apollo","Good",2);
         }
-        if(type.equals("FireStation")){
+        if("FireStation".equals(type)){
             return new FireStation("Firerescue","Good",3);
         }
-        if(type.equals("PoliceStation")){
+        if("PoliceStation".equals(type)){
             return new PoliceStation("Policestation1","excellent",3);
         }
         return null;
     }
-    public  People Getdetails(String type){
+    public People Getdetails(String type){
         return null;
     }
 }
